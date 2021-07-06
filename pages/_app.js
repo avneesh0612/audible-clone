77de6446ec751b3892b7a5a563cea761683c015a
Rozen2007 <deedirozen@gmail.com>
2021-07-06 16:11:17 +0530

    added some animation

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,25 @@
 import '../styles/globals.css'
+import { motion } from "framer-motion";
 
 function MyApp({ Component, pageProps }) {
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2
+      }
+    }
+  };
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1
+    }
+  };
   return <Component {...pageProps} />
 }
 

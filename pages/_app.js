62f4419beb0e22063a460abd08c605b563f6ad48
Avneesh Agarwal <mamtajalan22@gmail.com>
2021-07-06 16:48:58 +0530

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,26 @@
 import "../styles/globals.css";
 import { motion } from "framer-motion";
 
-function MyApp({ Component, pageProps, router }) {
-  return (
-    <motion.div
-      key={router.route}
-      initial="initial"
-      animate="animate"
-      variants={{
-        initial: {
-          opacity: 0,
-        },
-        animate: {
-          opacity: 1,
-        },
-      }}
-    >
-      <Component {...pageProps} />
-    </motion.div>
-  );
+function MyApp({ Component, pageProps }) {
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;

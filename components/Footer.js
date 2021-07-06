3758c24f5c591d979e3f50a0892c2eb27bf37526
Monwar <monwar259@gmail.com>
2021-07-06 17:50:17 +0100

    added items onto index page

---
+++ components/Footer.js
@@ -0,0 +1,42 @@
+function Footer() {
+    return (
+        <div className="bg-gray-100 w-full p-10">
+            <div className="max-w-7xl mx-auto w-full grid sm:grid-cols-4 grid-flow-row-dense leading-loose">
+                <div className="mb-5">
+                    <p>Contact Us</p>
+                    <p>Help</p>
+                    <p>Business Enquiries</p>
+                    <p>Affiliates</p>
+                    <p>Mobile Site</p>
+                </div>
+                <div className="mb-5">
+                    <p>Audible Mobile Apps</p>
+                    <p>Gift Centre</p>
+                    <p>Redeem a Promotional Code</p>
+                    <p>Whispersync for Voice</p>
+                </div>
+                <div className="mb-5">
+                    <p>Best Sellets</p>
+                    <p>New Releases</p>
+                    <p>Foreign Language</p>
+                    <p>Mystery, Thriller & Suspense</p>
+                    <p>Science Ficiton & Fantasy</p>
+                    <p>Road Tripe Listens</p>
+                    <p>Summer Listens</p>
+                </div>
+                <div className="mb-5">
+                    <p>History</p>
+                    <p>Biographies & Memoirs</p>
+                    <p>Business & Careers</p>
+                    <p>Health & Wellness</p>
+                    <p>Literacture & Fiction</p>
+                    <p>Children's Audiobooks</p>
+                    <p>Comedy</p>
+                </div>
+            </div>
+            <hr className="my-5 border-gray-400" />
+        </div>
+    )
+}
+
+export default Footer

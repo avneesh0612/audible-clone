cd2c1179b552be0dc677934d13268adca2cdf56e
Avneesh Agarwal <mamtajalan22@gmail.com>
2021-07-08 16:10:45 +0530

    fixed loader

--- pages/signin.js
+++ pages/signin.js
@@ -4,19 +4,9 @@ import Image from "next/image";
 import Head from "next/head";
 import Header from "../components/Header";
 import { motion } from "framer-motion";
-import { useState, useEffect } from "react";
-import Loader from "../components/Loader";
 import Pulse from "react-reveal/Pulse";
 
 export default function SignIn({ providers }) {
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, []);
-
   const container = {
     hidden: { opacity: 1, scale: 0 },
     visible: {
@@ -52,40 +42,34 @@ export default function SignIn({ providers }) {
             <link rel="icon" href="icon.jpg" />
           </Head>
 
-          {loading ? (
-            <Loader />
-          ) : (
-            <>
-              <Header />
-              <Pulse>
-                <div className="w-80 h-[70vh] mx-auto grid place-items-center bg-gray-100 mt-5 rounded-lg item">
-                  <Image
-                    width={140}
-                    height={54}
-                    objectFit="contain"
-                    src="https://m.media-amazon.com/images/G/31/audibleweb/arya/navigation/audible_logo._CB490888215_.svg"
-                    alt="logo"
-                  />
-                  <div>
-                    <div>
-                      {Object.values(providers).map((provider) => {
-                        return (
-                          <div key={provider.name}>
-                            <button
-                              className="button"
-                              onClick={() => signIn(provider.id)}
-                            >
-                              Sign in with {provider.name}
-                            </button>
-                          </div>
-                        );
-                      })}
-                    </div>
-                  </div>
+          <Header />
+          <Pulse>
+            <div className="w-80 h-[70vh] mx-auto grid place-items-center bg-gray-100 mt-5 rounded-lg item">
+              <Image
+                width={140}
+                height={54}
+                objectFit="contain"
+                src="https://m.media-amazon.com/images/G/31/audibleweb/arya/navigation/audible_logo._CB490888215_.svg"
+                alt="logo"
+              />
+              <div>
+                <div>
+                  {Object.values(providers).map((provider) => {
+                    return (
+                      <div key={provider.name}>
+                        <button
+                          className="button"
+                          onClick={() => signIn(provider.id)}
+                        >
+                          Sign in with {provider.name}
+                        </button>
+                      </div>
+                    );
+                  })}
                 </div>
-              </Pulse>
-            </>
-          )}
+              </div>
+            </div>
+          </Pulse>
         </div>
       </motion.div>
     </motion.div>

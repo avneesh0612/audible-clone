2599ff0f000a3a8dfca49356da397a5980243ca4
Max Programming <ui.sabuwala@gmail.com>
2021-07-17 18:37:30 +0530

    ✨ Progress bar on page change

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
-import "../styles/globals.css";
+import "styles/globals.css";
+import "styles/nprogress.css";
+import nProgress from "nprogress";
+import Router from "next/router";
 
+// Progress bar
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeComplete", nProgress.done);
+Router.events.on("routeChangeError", nProgress.done);
 
 function MyApp({ Component, pageProps }) {
-
   return <Component {...pageProps} />;
 }
 

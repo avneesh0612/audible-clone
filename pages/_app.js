0823977a90a4bae5ccff2d5a1427ca0d18d47058
Avneesh Agarwal <mamtajalan22@gmail.com>
2021-07-17 20:18:19 +0530

    auth upgrade

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "styles/globals.css";
 import "styles/nprogress.css";
 import nProgress from "nprogress";
 import Router from "next/router";
+import { Provider } from "next-auth/client";
 
 // Progress bar
 Router.events.on("routeChangeStart", nProgress.start);
@@ -9,7 +10,11 @@ Router.events.on("routeChangeComplete", nProgress.done);
 Router.events.on("routeChangeError", nProgress.done);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
